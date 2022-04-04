22defb1a4b7838c3625f7dbcb36c6a2484e20791
IktaS <imamrafiia@gmail.com>
2022-04-04 21:03:03 +0700

    bat perch

---
+++ Assets/Scripts/Enemy/BatPerch.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatPerch : MonoBehaviour
+{
+    [SerializeField] private Vector2 radiusRange;
+    [SerializeField] private float yOffset;
+    [SerializeField] private int maxNode;
+
+    private List<GameObject> nodes = new List<GameObject>();
+
+    void Start()
+    {
+        for (int i = 0; i < maxNode; i++)
+        {
+            var randRadius = Random.Range(radiusRange.x, radiusRange.y);
+            var point = Random.onUnitSphere * randRadius;
+            point.y = Mathf.Abs(point.y) + yOffset;
+            var node = new GameObject("Node " + i);
+            nodes.Add(node);
+            node.transform.position = transform.position + point;
+            node.transform.SetParent(transform);
+        }
+    }
+}
