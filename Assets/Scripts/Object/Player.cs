1d12b144396cf19696c545710fb8bf2fb95f882e
IktaS <imamrafiia@gmail.com>
2022-04-13 23:03:22 +0700

    player can take projectile damage

--- Assets/Scripts/Object/Player.cs
+++ Assets/Scripts/Object/Player.cs
@@ -6,6 +6,6 @@ public class Player : Entity, IProjectileHittable
 {
     public virtual void HitByProjectile(Projectile p, Vector3 power)
     {
-        Debug.Log("hit by " + p.name);
+        TakeDamage(p.GetDamage());
     }
 }
