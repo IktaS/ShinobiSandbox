6d1d2c299727a05312362c991fe6836f25fd14af
Imam Rafii <imamrafiia@gmail.com>
2022-04-29 22:09:28 +0700

    adding spawner logic

--- Assets/Scripts/Enemy/SpawnManager.cs
+++ Assets/Scripts/Enemy/SpawnManager.cs
@@ -7,7 +7,11 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private List<SpawnManagerDifficulty> difficulties = new List<SpawnManagerDifficulty>();
 
     [SerializeField] private List<Spawner> wolfSpawners = new List<Spawner>();
+    [SerializeField] private List<Spawner> woodWolfSpawners = new List<Spawner>();
+    [SerializeField] private List<Spawner> metalWolfSpawners = new List<Spawner>();
     [SerializeField] private List<Spawner> batSpawners = new List<Spawner>();
+    [SerializeField] private List<Spawner> woodBatSpawners = new List<Spawner>();
+    [SerializeField] private List<Spawner> metalBatSpawners = new List<Spawner>();
     [SerializeField] private List<Spawner> bombSpawners = new List<Spawner>();
 
     [SerializeField] private List<NodeRepository> nodeRepositories = new List<NodeRepository>();
@@ -29,7 +33,11 @@ public class SpawnManager : MonoBehaviour
     void OnEnable()
     {
         wolfSpawners.ForEach((spawner) => spawner.gameObject.SetActive(true));
+        woodWolfSpawners.ForEach((spawner) => spawner.gameObject.SetActive(true));
+        metalWolfSpawners.ForEach((spawner) => spawner.gameObject.SetActive(true));
         batSpawners.ForEach((spawner) => spawner.gameObject.SetActive(true));
+        woodBatSpawners.ForEach((spawner) => spawner.gameObject.SetActive(true));
+        metalBatSpawners.ForEach((spawner) => spawner.gameObject.SetActive(true));
         bombSpawners.ForEach((spawner) => spawner.gameObject.SetActive(true));
         Initialize();
     }
@@ -37,7 +45,11 @@ public class SpawnManager : MonoBehaviour
     void OnDisable()
     {
         wolfSpawners.ForEach((spawner) => spawner.gameObject.SetActive(false));
+        woodWolfSpawners.ForEach((spawner) => spawner.gameObject.SetActive(false));
+        metalWolfSpawners.ForEach((spawner) => spawner.gameObject.SetActive(false));
         batSpawners.ForEach((spawner) => spawner.gameObject.SetActive(false));
+        woodBatSpawners.ForEach((spawner) => spawner.gameObject.SetActive(false));
+        metalBatSpawners.ForEach((spawner) => spawner.gameObject.SetActive(false));
         bombSpawners.ForEach((spawner) => spawner.gameObject.SetActive(false));
     }
 
@@ -72,7 +84,11 @@ public class SpawnManager : MonoBehaviour
         difficulty.SetSeed(difficulty.seed);
         nodeRepositories.ForEach((repo) => repo.SetSeed(difficulty.seed));
         wolfSpawners.ForEach((spawner) => spawner.SetDifficulty(difficulty.wolf));
+        woodWolfSpawners.ForEach((spawner) => spawner.SetDifficulty(difficulty.woodwolf));
+        metalWolfSpawners.ForEach((spawner) => spawner.SetDifficulty(difficulty.metalwolf));
         batSpawners.ForEach((spawner) => spawner.SetDifficulty(difficulty.bat));
+        woodBatSpawners.ForEach((spawner) => spawner.SetDifficulty(difficulty.woodbat));
+        metalBatSpawners.ForEach((spawner) => spawner.SetDifficulty(difficulty.metalbat));
         bombSpawners.ForEach((spawner) => spawner.SetDifficulty(difficulty.bomb));
     }
 }
--- Assets/Scripts/Enemy/SpawnManagerDifficulty.cs
+++ Assets/Scripts/Enemy/SpawnManagerDifficulty.cs
@@ -9,13 +9,21 @@ public class SpawnManagerDifficulty : ScriptableObject
     public EntitySpawnDifficulty wolf;
     public EntitySpawnDifficulty bat;
     public EntitySpawnDifficulty bomb;
+    public EntitySpawnDifficulty woodwolf;
+    public EntitySpawnDifficulty metalwolf;
+    public EntitySpawnDifficulty woodbat;
+    public EntitySpawnDifficulty metalbat;
     public float nextDifficultyTimer = 360f;
 
     public void SetSeed(int _seed)
     {
         seed = _seed;
         wolf.SetSeed(seed);
+        woodwolf.SetSeed(seed);
+        metalwolf.SetSeed(seed);
         bat.SetSeed(seed);
+        woodbat.SetSeed(seed);
+        metalbat.SetSeed(seed);
         bomb.SetSeed(seed);
     }
 }
